a8218d1fac1b24e6b9d07f51f54175f78a9f61d0
SanSun <sandy.sundram@gmail.com>
2018-06-20 20:38:32 -0700

    buttons without same value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,27 +6,29 @@ var greenButton = 0;
 var totalPlayerScore = 0;
 var totalWins = 0;
 var totalLosses = 0;
-
+var firstClick = 0; 
 
 randomNumGenerator();
 
 function randomNumGenerator(){
     totalPlayerScore = 0;
+    firstClick = 0;
 
-    randomNumbertoGuess = Math.ceil(Math.random()*(120 - 18 + 1)) + 18;
-    redButton = Math.ceil(Math.random()*12);
-    blueButton = Math.ceil(Math.random()*12);
-    yellowButton = Math.ceil(Math.random()*12);
-    greenButton = Math.ceil(Math.random()*12);
+    randomNumbertoGuess = Math.ceil(Math.random()*(120 - 19 + 1)) + 19;
+    do{
+        redButton = Math.ceil(Math.random()*12);
+        blueButton = Math.ceil(Math.random()*12);
+        yellowButton = Math.ceil(Math.random()*12);
+        greenButton = Math.ceil(Math.random()*12);
+    }while((redButton == blueButton) || (redButton == yellowButton) || (redButton == greenButton) 
+        || (blueButton == yellowButton) || (blueButton == greenButton) || (yellowButton == greenButton))
 
-    console.log(randomNumbertoGuess);
     $("#guessScore").text(randomNumbertoGuess);
     $("#redCrystal").attr('data-value',redButton);
     $("#blueCrystal").attr('data-value',blueButton);
     $("#yellowCrystal").attr('data-value',yellowButton);
     $("#greenCrystal").attr('data-value',greenButton);
 
-    // $("#result").text('');
     $("#totalScore").text(totalPlayerScore);
 
     
@@ -36,14 +38,15 @@ $('button').on('click',checkNum);
 
 function checkNum(){
     var buttonClicked = $(this);
-    console.log(totalPlayerScore);
+
+    firstClick++;
+    if (firstClick == 1){
+        removeResult();
+    }
 
     totalPlayerScore = parseInt(buttonClicked.attr('data-value')) + totalPlayerScore;
-    // (buttonClicked.attr('data-value')) + totalPlayerScore;
 
     $('#totalScore').text(totalPlayerScore);
-
-    console.log(totalPlayerScore);
 
     if (randomNumbertoGuess < totalPlayerScore){
         $("#result").text('You Lose');
@@ -58,4 +61,8 @@ function checkNum(){
         randomNumGenerator();
     }
 
+}
+
+function removeResult(){
+    $("#result").text('');
 }
